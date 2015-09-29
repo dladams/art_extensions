26f45983a1c7498d4a5da63be255692d6b7a4c20
David Adams <dladams@bnl.gov>
2015-09-29 13:22:20 -0400

    Add test_EventID.

---
+++ test/event/test_EventID.cxx
@@ -0,0 +1,60 @@
+// test_EventID.cxx
+
+// David Adams
+// September 2015
+//
+// This test demonstrates how to use art::Event outside art.
+// It is not easy to do.
+//
+// Suggestions for improving this are welcome.
+
+#include "art/Framework/Principal/Event.h"
+#include "art/Persistency/Provenance/ProductMetaData.h"
+#include "art/Persistency/Provenance/ReleaseVersion.h"
+#include "art/Persistency/Provenance/ProcessConfiguration.h"
+#include "art/Framework/Principal/EventPrincipal.h"
+#include "art/Persistency/Provenance/ModuleDescription.h"
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+using art::EventID;
+
+//**********************************************************************
+
+int test_EventID() {
+  const string myname = "test_EventID: ";
+  cout << myname << "Starting test" << endl;
+#ifdef NDEBUG
+  cout << myname << "NDEBUG must be off." << endl;
+  abort();
+#endif
+  string line = "-----------------------------";
+  string scfg;
+
+  cout << myname << line << endl;
+  cout << myname << "Create event ID." << endl;
+  EventID eid(101, 0, 151);
+  cout << "    Run: " << eid.run() << endl;
+  cout << "  Event: " << eid.event() << endl;
+
+  cout << myname << line << endl;
+  cout << myname << "Checking event ID." << endl;
+  assert( eid.run() == 101 );
+  assert( eid.subRun() == 0 );
+  assert( eid.event() == 151 );
+
+  cout << myname << line << endl;
+  cout << "Done." << endl;
+  return 0;
+}
+
+//**********************************************************************
+
+int main() {
+  return test_EventID();
+}
+
+//**********************************************************************
