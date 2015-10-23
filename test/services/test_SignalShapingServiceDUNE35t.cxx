c9727d4bdedc6586f5139a45094fd2b15bb24829
David Adams <dladams@bnl.gov>
2015-10-23 14:14:59 -0400

    Add workaround for test crash.

--- test/services/test_SignalShapingServiceDUNE35t.cxx
+++ test/services/test_SignalShapingServiceDUNE35t.cxx
@@ -9,18 +9,21 @@
 #include "dune/Utilities/SignalShapingServiceDUNE35t.h"
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <iomanip>
 #include "AXService/ArtServiceHelper.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "Utilities/LArFFT.h"
 #include "Geometry/Geometry.h"
+#include "TH1.h"
 
 using std::string;
 using std::cout;
 using std::endl;
 using std::vector;
 using std::setw;
+using std::ofstream;
 
 int test_SignalShapingServiceDUNE35t() {
   const string myname = "test_SignalShapingServiceDUNE35t: ";
@@ -31,6 +34,10 @@ int test_SignalShapingServiceDUNE35t() {
 #endif
   string line = "-----------------------------";
   string scfg;
+
+  cout << myname << line << endl;
+  cout << "Prevent Root from managing histograms." << endl;
+  TH1::AddDirectory(kFALSE);
 
   cout << myname << line << endl;
   cout << myname << "Fetch art service helper." << endl;
@@ -46,8 +53,12 @@ int test_SignalShapingServiceDUNE35t() {
   scfg += "services: {\n";
   scfg += "user: @local::dune35t_services\n";
   scfg += "}";
+  {
+    ofstream ofile("test_SignalShapingServiceDUNE35t_services.fcl");
+    ofile << scfg << endl;
+  }
   // This works if the file contains something like the above text.
-  scfg = "myservices.fcl";
+  scfg = "test_SignalShapingServiceDUNE35t_services.fcl";
   isFile = true;
   cout << myname << "Configuration: " << scfg << endl;
   assert( ash.addService("SignalShapingServiceDUNE35t", scfg, isFile) == 0 );
@@ -65,6 +76,7 @@ int test_SignalShapingServiceDUNE35t() {
   assert( ash.addService("LArProperties", scfg, isFile) == 0 );
   assert( ash.addService("DatabaseUtil", scfg, isFile) == 0 );
 
+  cout << myname << line << endl;
   cout << myname << "Print the services." << endl;
   ash.print();
 
@@ -92,24 +104,44 @@ int test_SignalShapingServiceDUNE35t() {
 
   cout << myname << line << endl;
   cout << myname << "Create raw data." << endl;
+  unsigned int chan = 450;   // Must be a collection plane.
   vector<float> raw;
   for ( int i=0; i<20; ++i ) raw.push_back(0.0);
+  raw.push_back(1.0);
+  raw.push_back(1.0);
   raw.push_back(2.0);
+  raw.push_back(2.0);
+  raw.push_back(3.0);
   raw.push_back(4.0);
-  raw.push_back(10.0);
+  raw.push_back(5.0);
   raw.push_back(6.0);
-  raw.push_back(4.0);
-  raw.push_back(2.0);
+  raw.push_back(8.0);
+  raw.push_back(10.0);
+  raw.push_back(10.0);
+  raw.push_back(13.0);
+  raw.push_back(16.0);
+  raw.push_back(20.0);
+  raw.push_back(25.0);
+  raw.push_back(30.0);
+  raw.push_back(34.0);
+  raw.push_back(33.0);
+  raw.push_back(27.0);
+  raw.push_back(21.0);
+  raw.push_back(14.0);
+  raw.push_back( 8.0);
+  raw.push_back( 3.0);
+  raw.push_back( 2.0);
+  for ( int i=0; i<20; ++i ) raw.push_back(0.0);
   assert( xformSize > raw.size() );
 
   cout << endl;
   cout << myname << "Deconvolute." << endl;
-  vector<float> holder(xformSize);;
-  for ( unsigned int i=0; i<raw.size(); ++i ) holder[i] = raw[i];
-  psss->Deconvolute(0, holder);
+  vector<float> decon(xformSize);;
+  for ( unsigned int i=0; i<raw.size(); ++i ) decon[i] = raw[i];
+  psss->Deconvolute(chan, decon);
   cout << "      Raw     Decon" << endl;
   for ( int unsigned i=0; i<raw.size(); ++i ) {
-    cout << setw(10) << raw[i] << setw(10) << holder[i] << endl;
+    cout << setw(10) << raw[i] << setw(10) << decon[i] << endl;
   }
   cout << myname << line << endl;
   cout << myname << "Close services." << endl;
